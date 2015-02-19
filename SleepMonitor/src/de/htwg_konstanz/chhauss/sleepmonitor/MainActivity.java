68ff7eaafcd5b9c6a52aba24280e44c06ab252f6
Chris <Chris@R2D2>
2015-02-19 14:38:54 +0100

    sound aufnehmen und wieder abspielen funktioniert

--- SleepMonitor/src/de/htwg_konstanz/chhauss/sleepmonitor/MainActivity.java
+++ SleepMonitor/src/de/htwg_konstanz/chhauss/sleepmonitor/MainActivity.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -18,6 +20,13 @@ public class MainActivity extends Activity {
 	private MediaPlayer mediaPlayer;
 	private MediaRecorder recorder;
 	private String OUTPUT_FILE;
+	private File record;
+	
+	private boolean recording;
+	private boolean playing;
+	
+	private Button recordBtn;
+	private Button playBtn;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,15 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
     	OUTPUT_FILE = Environment.getExternalStorageDirectory() + "/sleepRecord.3gpp";
+    	recording = false;
+    	playing = false;
+    	recordBtn = (Button) findViewById(R.id.recordBtn);
+    	playBtn = (Button) findViewById(R.id.playBtn);
+    	record = new File(OUTPUT_FILE);
+    	
+    	if(!record.exists()) {
+    		playBtn.setEnabled(false);
+    	}
     	
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -49,44 +67,46 @@ public class MainActivity extends Activity {
     
     public void buttonToggled(View v)  {
     	switch(v.getId()) {
-    	case R.id.startRecording:
-    		try {
-    			startRecording();
-    		} catch(Exception e) {
-    			e.printStackTrace();
+    	case R.id.recordBtn:
+    		if(!recording){
+    			try {
+    				startRecording();
+    			} catch(Exception e) {
+    				e.printStackTrace();
+    			}
+    		} else {
+    			try {
+        			System.out.println("2");
+        			stopRecording();
+        		} catch(Exception e) {
+        			e.printStackTrace();
+        		}
     		}
-    	case R.id.stopRecording:
-    		try {
-    			stopRecording();
-    		} catch(Exception e) {
-    			e.printStackTrace();
+    		break;
+    	case R.id.playBtn:
+    		if(!playing) {
+    			try {
+    				System.out.println("3");
+    				playRecord();
+    			} catch(Exception e) {
+    				e.printStackTrace();
+    			}
+    		} else {
+    			try {
+    				System.out.println("4");
+    				stopPlayback();
+    			} catch(Exception e) {
+    				e.printStackTrace();
+    			}
     		}
-    	case R.id.playRecord:
-    		try {
-    			playRecord();
-    		} catch(Exception e) {
-    			e.printStackTrace();
-    		}
-    	case R.id.stopPlayback:
-    		try {
-    			stopPlayback();
-    		} catch(Exception e) {
-    			e.printStackTrace();
-    		}
+    		break;
     	}
     }
     
     private void startRecording() throws IllegalStateException, IOException {
-    	// Stop recording
-		if(recorder != null) {
-			recorder.release();
-		}
-		
-		// delete output-file
-		File outFile = new File(OUTPUT_FILE);
-		
-		if(outFile.exists()) {
-			outFile.delete();
+		// delete old record
+		if(record.exists()) {
+			record.delete();
 		}
 		
 		// Recording
@@ -97,28 +117,49 @@ public class MainActivity extends Activity {
 		recorder.setOutputFile(OUTPUT_FILE);
 		recorder.prepare();
 		recorder.start();
+		
+		recording = true;
+		recordBtn.setText(R.string.stopRecording);
+		playBtn.setEnabled(false);
 	}
     
 	private void stopRecording() {
-		if(recorder != null) {
-			recorder.stop();
-		}
+		recorder.stop();
+		recorder.release();
+		
+		recording = false;
+		recordBtn.setText(R.string.startRecording);
+		playBtn.setEnabled(true);
 	}
     
 	private void playRecord() throws IllegalStateException, IOException {
-		// Stop playback
-		if(mediaPlayer != null) {
-			mediaPlayer.release();
+		if(!record.exists()) {
+			return;  // No record available
 		}
 		
 		mediaPlayer = new MediaPlayer();
+		mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				stopPlayback();
+			}
+		});
+		
 		mediaPlayer.setDataSource(OUTPUT_FILE);
 		mediaPlayer.prepare();
 		mediaPlayer.start();
+		
+		playing = true;
+		recordBtn.setEnabled(false);
+		playBtn.setText(R.string.stopPlayback);
 	}
 	
 	private void stopPlayback() {
-		if(mediaPlayer != null)
-			mediaPlayer.stop();
+		mediaPlayer.stop();
+		mediaPlayer.release();
+		
+		playing = false;
+		recordBtn.setEnabled(true);
+		playBtn.setText(R.string.playRecord);
 	}
 }
